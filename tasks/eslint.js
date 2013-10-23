1012d973d49205aee598dd2ba144abe710e62286
Ian Christian Myers <ian@iancmyers.com>
2013-10-23 14:23:07 -0700

    Upgraded eslint version
    Upgraded the eslint version to 0.1.0-dev. In addition, I've removed `/tasks/conf` as the default eslint.json now errors instead of warns by default.

--- tasks/eslint.js
+++ tasks/eslint.js
@@ -2,7 +2,6 @@ var eslint = require("./lib/eslint"),
     path = require("path");
 
 var DEFAULTS = {
-  config: path.relative(process.cwd(), path.join(__dirname, "conf/eslint.json")),
   formatter: path.relative(process.cwd(), path.join(__dirname, "formatters/grunt.js"))
 };
 
